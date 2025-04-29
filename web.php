53494708125d2d62d3f1a0dfa8aaab523cee75a1
afiizzuddin <sp-mohdafiizzuddin.ayob@live.utm.my>
2025-04-30 02:37:52 +0800

    Add files via upload
    Add new route for report

---
+++ web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Livewire\Expense\Create;
+use App\Livewire\Expense\Edit;
+use App\Livewire\Expense\Index;
+use App\Livewire\Report\ExpenseReport;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
+
+Route::get('/', function () {
+    // return view('welcome');
+    return redirect()->route('login');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+
+    Route::prefix('expense')->group(function () {
+        Route::get('', Index::class)->name('expense.index');
+        Route::get('create', Create::class)->name('expense.create');
+        Route::get('edit/{id}', Edit::class)->name('expense.edit');
+
+   
+    });
+
+    Route::get('/report/expense', ExpenseReport::class)->name('report.expense');
+});
+
