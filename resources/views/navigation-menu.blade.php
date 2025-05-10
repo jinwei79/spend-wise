90926f13bd3f8083c84f38ee5b1933c73f9d6b48
Mohd Afi Izzuddin Ayob <102817785+devhope-spec@users.noreply.github.com>
2025-05-10 23:42:58 +0800

    change contact us route

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -88,7 +88,7 @@
                         {{ __('Report') }}
                     </x-nav-link>
                     
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('contact')" class="flex items-center">
+                    <x-nav-link href="{{ route('contactus') }}" :active="request()->routeIs('contact')" class="flex items-center">
                         <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
